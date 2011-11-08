facd4bddc86dc96d3d34b9c37f5a2ab130ffe303
xerces <dlcs.frank@gmail.com>
2011-11-08 14:16:27 -0400

    Ported FENcode over to ruby 1.9.1

---
+++ fencode.rb
@@ -0,0 +1,47 @@
+# Author: Francisco De La Cruz <dlcs.frank@gmail.com>
+# Date: Nov 8, 2011
+# Crude/Naive FEN array to algebraic notation converter
+require 'enumerator'
+require 'benchmark' 
+# For ruby < 1.9.1 uncomment the line below
+#require 'jcode'
+moves = []
+CHAR='_'
+FENS = ["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0",
+             "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
+             "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2",
+             "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"]
+=begin
+FENS = Array["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0",
+"rnbqkbnr/ppp1pppp/8/3p4/3P4/4P3/PPP2PPP/RNBQKBNR b KQkq - 0 2",
+"rnbqkb1r/ppp1pppp/5n2/3p4/3P4/4P3/PPP2PPP/RNBQKBNR w KQkq - 1 2"]
+=end
+
+FENS.collect { |fen|
+  fen[0,fen.index(/\s/)].split('/').each { |rank|
+    rank.gsub!(/\d/) {|d| CHAR*Integer(d) }
+  }
+}
+  .each_cons(2) { |a, b| 
+  rank=0
+  a.zip(b).each do |ra, rb|
+    8.times {|file|
+      ra_chr = ra[file]
+      rb_chr = rb[file]
+      act_rank = 8-rank
+      act_file = (file+65).chr
+      if ra_chr != rb_chr
+	if ra_chr == CHAR
+          if rb_chr.downcase == 'p'
+            moves.push("#{act_file}#{act_rank}")
+          else
+            moves.push("#{rb_chr}#{act_file}#{act_rank}")
+          end
+	end
+      end
+    }
+    rank+=1
+  end
+}
+# Just print them, for now !
+puts moves
