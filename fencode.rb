7f43778a638140b75e3aa908725133918d7d971f
xerces <dlcs.frank@gmail.com>
2011-11-09 16:59:47 -0400

    Added piece taking support. Castling support missing.

--- fencode.rb
+++ fencode.rb
@@ -6,38 +6,76 @@
 require 'enumerator'
 
 moves = []
-CHAR='_'
-FENS = ["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0",
-        "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
-        "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2",
-        "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"]
+FENS = ["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq -",
+        "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq -",
+        "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq -",
+        "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq -",
+        "rnbqkbnr/pp2pppp/3p4/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq -",
+        "rnbqkbnr/pp2pppp/3p4/2p5/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq -",
+        "rnbqkbnr/pp2pppp/3p4/8/3pP3/5N2/PPP2PPP/RNBQKB1R w KQkq -",
+        "rnbqkbnr/pp2pppp/3p4/8/3NP3/8/PPP2PPP/RNBQKB1R b KQkq -",
+        "rnbqkb1r/pp2pppp/3p1n2/8/3NP3/8/PPP2PPP/RNBQKB1R w KQkq -",
+        "rnbqkb1r/pp2pppp/3p1n2/8/3NP3/2N5/PPP2PPP/R1BQKB1R b KQkq -",
+        "rnbqkb1r/1p2pppp/p2p1n2/8/3NP3/2N5/PPP2PPP/R1BQKB1R w KQkq -",
+        "rnbqkb1r/1p2pppp/p2p1n2/8/3NP3/2N1B3/PPP2PPP/R2QKB1R b KQkq -",
+        "rnbqkb1r/1p3ppp/p2p1n2/4p3/3NP3/2N1B3/PPP2PPP/R2QKB1R w KQkq -",
+        "rnbqkb1r/1p3ppp/p2p1n2/4p3/4P3/1NN1B3/PPP2PPP/R2QKB1R b KQkq -"]
+
+
 =begin
 FENS = Array["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0",
 "rnbqkbnr/ppp1pppp/8/3p4/3P4/4P3/PPP2PPP/RNBQKBNR b KQkq - 0 2",
 "rnbqkb1r/ppp1pppp/5n2/3p4/3P4/4P3/PPP2PPP/RNBQKBNR w KQkq - 1 2"]
 =end
 
-FENS.map! { |fen| fen[0,fen.index(/\s/)].split('/').each { |rank|
-rank.gsub!(/\d/) {|d| CHAR*Integer(d)}}}.each_cons(2) { |a, b| rank=0
-  a.zip(b).each do |ra, rb| 8.times {|file| ra_chr = ra[file]
-      rb_chr = rb[file]
-      act_rank = 8-rank
-      act_file = (file+65).chr
-      if ra_chr != rb_chr
-	if ra_chr == CHAR
-          if rb_chr.downcase == 'p'
-            moves.push("#{act_file}#{act_rank}")
-          else
-            moves.push("#{rb_chr}#{act_file}#{act_rank}")
+def fens2pgn()
+
+end
+
+def fen2alg(fens)
+  moves = []
+  char='_'
+act_file = ''
+  fens.map! { |fen| fen[0,fen.index(/\s/)].split('/').each { |rank|
+      rank.gsub!(/\d/) {|d| char*Integer(d)}}}.each_cons(2) { |a, b| rank=0
+    a.zip(b).each do |ra, rb| 8.times {|file| ra_chr = ra[file]
+        # Board b, designated by rb is in the future
+        rb_chr = rb[file]
+        act_rank = 8-rank
+        old_file = act_file        
+        act_file = (file+65).chr
+
+        if ra_chr != rb_chr
+          if ra_chr == char
+            if rb_chr.downcase == 'p'
+              moves.push("#{act_file.downcase}#{act_rank}")
+            else
+              moves.push("#{rb_chr}#{act_file.downcase}#{act_rank}")
+            end
+          elsif ra_chr != char && rb_chr != char
+            # Take b position
+            puts "Attack: #{ra_chr} <-> #{rb_chr}"
+            if rb_chr.downcase == 'p'
+              # This is a pawn move, get other side file
+              moves.push("#{old_file.downcase}x#{act_file.downcase}#{act_rank}")
+            else
+              moves.push("#{rb_chr}x#{act_file.downcase}#{act_rank}")
+            end
           end
-	end
-      end
-    }
-    rank+=1
-  end
-}
+        end
+      }
+      rank+=1
+    end
+  }
+  moves
+end
+
+
+alg = fen2alg(FENS)
+
 # Just print them, for now !
 puts "==Generated Boards=="
-puts FENS
+FENS.each_with_index {|f,i| puts "NEW BOARD#{i}: " ; puts f ; puts "\n\n"  }
+
 puts "==Calculated Moves=="
-puts moves
+puts alg
